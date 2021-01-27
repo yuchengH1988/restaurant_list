a2d02c88d1ae60fc36bc027d7f3b4a130bd100a7
yuchengH1988 <yucheng.h.1988@gmail.com>
2021-01-27 17:45:11 +0800

    feat: add create function

--- app.js
+++ app.js
@@ -2,26 +2,26 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
-// require express-handlebars here
 const exphbs = require('express-handlebars')
 const Restaurant = require('./models/restaurant')
+const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/restaurant_list', { useNewUrlParser: true, useUnifiedTopology: true })
-// setting template engine
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
 
-// setting static files
-app.use(express.static('public'))
 const db = mongoose.connection
-
 db.on('error', () => {
   console.log('mongodb error!')
 })
 db.once('open', () => {
   console.log('mongodb connected')
 })
+
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+
 // routes setting
 app.get('/', (req, res) => {
   Restaurant.find()
@@ -29,6 +29,18 @@ app.get('/', (req, res) => {
     .then(restaurants => res.render('index', { restaurants }))
     .catch(error => console.log(error))
   // res.render('index', { restaurants: restaurantList.results })
+})
+
+// routes setting add new function
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new')
+})
+app.post('/restaurants', (req, res) => {
+  if (!req.body.image) { req.body.image = 'https://www.publicdomainpictures.net/pictures/280000/velka/not-found-image-15383864787lu.jpg' }
+  const newItem = req.body
+  return Restaurant.create(newItem)
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
 app.get('/restaurants/:id', (req, res) => {
--- models/restaurant.js
+++ models/restaurant.js
@@ -13,14 +13,3 @@ const restaurantSchema = new Schema({
   description: String,
 })
 module.exports = mongoose.model('Restaurant', restaurantSchema)
-
-//        "id": 2,
-// "name": "梅子鰻蒲燒專賣店",
-//   "name_en": "Umeko Japanese Unagi House",
-//     "category": "日本料理",
-//       "image": "https://assets-lighthouse.s3.amazonaws.com/uploads/image/file/5628/02.jpg",
-//         "location": "台北市中山區林森北路 107 巷 8 號",
-//           "phone": " 02 2521 2813",
-//             "google_map": "https://goo.gl/maps/cUJEmFSRKyH2",
-//               "rating": 4.3,
-//                 "description": "鰻魚、鰻魚飯、真空鰻魚"
