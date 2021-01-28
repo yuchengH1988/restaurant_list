13e2269bd440e13c681de8470f47fcee816f7f64
yuchengH1988 <yucheng.h.1988@gmail.com>
2021-01-28 20:40:49 +0800

    feat:add delete function

--- app.js
+++ app.js
@@ -73,6 +73,16 @@ app.post('/restaurants/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+//Delete route
+app.post('/restaurants/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
   const restaurants = restaurantList.results.filter(restaurant => {
